6e2c45719cb3af67b0e69b32d98604663f0e99a0
tauomicronmu <tauomicronmurs@gmail.com>
2018-06-20 17:55:38 +0100

    this might be done :^)

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <pthread.h>
 
+const size_t TOTAL_WORK = 990000;
 const size_t NUM_WORKERS = 99;
-size_t SERVER_FINISHED = 0;
+long long double PI = 0;
 
-int client(void);
+long long WORK_PER_WORKER;
+
+int client(long long start_n);
 int server(void);
-int handleRequest(MPIStatus);
+long double calc(long long start_n);
 
 int main(int argc, char ** argv) {
     MPI_Init(&argc, &argv);
@@ -16,8 +18,13 @@ int main(int argc, char ** argv) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    long long N = 0;
+    WORK_PER_WORKER = TOTAL_WORK/NUM_WORKERS;
+
     if(rank == 0) server();
-    else client();
+    else {
+        client(N);
+        N += WORK_PER_WORKER;
 
     MPI_Finalize();
 
@@ -25,85 +32,52 @@ int main(int argc, char ** argv) {
 }
 
 /*
-    The server has some central data structure, and the clients can make requests that alter or return that
-    data structure.
+    The server holds an int, and listens for read/write
+    requests from clients. 
 */
 int server(void) {
-    size_t WORKERS_ONGOING = NUM_WORKERS;
- 
-    // TODO: work out how to do ongoing workers?
-    while(!SERVER_FINISHED) { 
-        // TODO: add a check to see if the work is done!
-        // NB: this should alter SERVER_FINISHED
-
-        // Wait for a client to send a request and handle it in a new thread
-        size_t result;
-        MPI_Status status;
-        MPI_Recv(&result, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+     
+    long long WORKERS_FINISHED = 0;
    
-        // Spin up a new pthread to handle the client request 
-        pthread_t client_thread;
-        pthread_create(&client_thread, NULL, handleRequest, &status);
-        pthread_join(client_thread, NULL);         
- 
-    }     
-
-    // Once the server has finished, wait for all 99 clients to poll the server for status
-    while(WORKERS_ONGOING > 0) {
-        // Wait for a client to send a request and handle it in a new thread
-        size_t result;
+    while(WORKERS_FINISHED < NUM_WORKERS) {
+        // Wait for a worker to respond with their workload and add it to PI
         MPI_Status status;
-        MPI_Recv(&result, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        // We don't care about any requests that aren't polling for status
-        if(status.MPI_TAG != 0) continue;
-
-        // Spin up a new pthread to handle the client request 
-        pthread_t client_thread;
-        pthread_create(&client_thread, NULL, handleRequest, &status);
-        pthread_join(client_thread, NULL);
-        
-        WORKERS_ONGOING--;
+        long double response;
+        MPI_Recv(&response, 1, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        printf("Received response from client\n");
+        PI += response;
+        WORKERS_FINISHED++;
     }
-  
-    // TODO: provide any required output before the program exits
 
+    printf("Pi is: %lld\n", (long long double)((4.0*PI)/N));
+    
     return 0;
 }
-
-/* 
-    Handles a client request 
-*/
-int handleRequest(MPI_Status status) {
-    switch(status.MPI_TAG) {
-        // TODO: Add cases here for each requests
-        case 0:
-            // In this case, the client is polling to see if the work is done.
-            // Hence, return the SERVER_FINISHED int (0 if not done, 1 if done)    
-            MPI_Send(&SERVER_FINISHED, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD);
-            return 0;
-        default:
-            return 0;
-    }    
-}
-
 
 /*
-   The client sends requests to the server
+   The client asynchronously reads and writes to the int
+   stored in the server. 
 */ 
-int client(void) {
-    size_t WORK_FINISHED = 0; 
- 
-    while(!WORK_FINISHED) {
-        // First, poll the server to check we still have work!
-        MPI_Send(&WORK_FINISHED, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD);
-        
-        // Then get the result from the server, and update WORK_TO_BE_DONE
-        MPI_Status status;
-        MPI_Recv(&WORK_FINISHED, 1, MPI_UNSIGNED, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+int client(long long start_n) {
+    // Calculate pi between n and n + WORK_PER_WORKER
+    long double retval = calc(start_n);
+   
+    // Send retval back to the server
+    MPI_Send(&retval, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
 
-        // TODO: Do any required work
-    }
-
+    printf("Client exiting\n");
     return 0;
 }
+
+long double calc(long long start_n) {
+    long double a,b,c,calc = 0.0;
+    for (long long i = start_n; i < start_n + WORK_PER_WORKER; i++) {
+        c = ((long double) i - 0.5) / ((long double) TOTAL_WORK);
+        b = pow(c, 2.0);
+        a = 1.0 + b;
+        calc += 1.0/a;
+    }
+    return calc;
+}
+
+
